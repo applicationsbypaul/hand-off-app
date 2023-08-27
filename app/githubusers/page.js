13b86f5b612436cd99445fa6a8e5fcd33610fd5b
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2023-08-27 09:52:33 -0500

    map items from database to table

--- app/githubusers/page.js
+++ app/githubusers/page.js
@@ -1,17 +1,47 @@
+import Link from "next/link";
+
 async function fetchGitHubUsers() {
-  //const res = await fetch("http://localhost:5232/api/Employee");
-  const res = await fetch("https://api.github.com/search/users?q=greg");
+  const res = await fetch("http://localhost:5232/api/Employee");
+  //const res = await fetch("https://api.github.com/search/users?q=greg");
   const json = await res.json();
-  return json.items;
+  return json;
 }
 
 const GitHubUsersPage = async () => {
   const users = await fetchGitHubUsers();
-  console.log(users);
 
   return (
-    <div>
-      <h1>GitHub Users Page</h1>
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th> <th>URL</th> <th>Repos</th> <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={user.avatar_url} />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold"> {user.employeeFirstName}</div>
+                    <div className="text-sm opacity-50">
+                      {user.employeeLastName}
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td></td>
+              <th> Go to Repos </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
