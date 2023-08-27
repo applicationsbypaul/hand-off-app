b1af7162e1f8efb38290a3c704d7ba1c16bb0632
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2023-08-26 22:16:56 -0500

    Adding a new page and link to the page

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Maybe talk about the app and the purpose</p>
+    </div>
+  );
+};
+export default AboutPage;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <h2>Maybe I will do a floaty menu that I can select things quick</h2>
       <button className="btn btn-primary">Button</button>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
