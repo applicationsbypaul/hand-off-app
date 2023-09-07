1a1e0d39ef9e4221c27174d1651bd525c008cef1
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2023-09-06 23:27:25 -0500

    reformat

--- app/components/Books.js
+++ app/components/Books.js
@@ -1,14 +1,12 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import LoadingPage from "../loading";
-
 async function getBooks() {
   const res = await fetch("http://localhost:3000/api/books");
   const json = await res.json();
   return json;
 }
-
 const Books = async () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -18,19 +16,20 @@ const Books = async () => {
       setLoading(false);
     });
   }, []);
-  
-  if(loading){return <LoadingPage/>}
-
+  if (loading) {
+    return <LoadingPage />;
+  }
   return (
     <div>
+      
       <h1>Books</h1>
       {books.map((book) => (
-        <div key={book.id}>
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure>
+        <div key={book.id}>         
+          <div className="card w-96 bg-base-100 shadow-xl">           
+            <figure>             
               <img src={book.img} width="200" height="150" />
             </figure>
-            <div className="card-body">
+            <div className="card-body">              
               <h2 className="card-title">{book.id}</h2> <p>{book.title}</p>
               <div className="card-actions justify-end">
                 <Link href={book.link} className="btn btn-primary">
